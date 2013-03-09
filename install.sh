7c16f1108a42cb84007561f10f99f6aef5192568
liwei <henyouqian@gmail.com>
2013-03-09 19:06:42 +0800

    lw

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+apt-get install python-pip
+pip install virtualenv
 virtualenv env
 . env/bin/activate
 
@@ -5,7 +7,11 @@ pip install tornado
 pip install simplejson
 
 apt-get install python-dev
-easy_install psycopg2
+#easy_install psycopg2
 
 apt-get build-dep python-mysqldb
 pip install mysql-python
+
+cd brukva
+python setup.py install
+cd ..
