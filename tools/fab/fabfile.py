ffbc4a84abea402831c58b121c8a3af524145b3c
liwei <henyouqian@gmail.com>
2013-05-05 17:10:39 +0800

    lw

--- tools/fab/fabfile.py
+++ tools/fab/fabfile.py
@@ -1,6 +1,7 @@
 from fabric.api import env, sudo, put, local, get
 
-env.hosts = ["121.199.15.152", "121.199.15.151"]
+env.hosts = ["121.199.15.152"]
+# env.hosts = ["121.199.15.152", "121.199.15.151"]
 # env.hosts = ["42.121.107.155"]
 # env.hosts = ["localhost"]
 env.user = 'root'
--- tools/fab/wh_deploy.py
+++ tools/fab/wh_deploy.py
@@ -3,7 +3,7 @@ import os
 work_dir = "/home/wh/cdgm"
 os.chdir(work_dir)
 
-# os.system("git pull")
+os.system("git pull")
 
 os.system("env/bin/python daemon.py stop")
 os.system("env/bin/python daemon.py start")
