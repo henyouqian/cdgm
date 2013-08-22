091b5b359d2e37c590bce241cbf8dcab85c18ac8
liwei <henyouqian@gmail.com>
2013-08-22 15:40:45 +0800

    lw

--- gamedata.py
+++ gamedata.py
@@ -12,7 +12,7 @@ INIT_GOLD = 10000
 BAND_NUM = 3
 INIT_CARDS = [[130, 10]]	#[[proto, level], ...]
 INIT_BANDS = [{"formation":1, "members":[None, None, None, None, None, None]} for __ in xrange(BAND_NUM)]
-INIT_ITEMS = {1:50, 2:50, 3:50, 10:50}	# {3:5, 5:34} => id:count		7:bronzeCoin, 8:silverCoin
+INIT_ITEMS = {1:50, 2:50, 3:50, 8:3, 10:50}	# {3:5, 5:34} => id:count		7:bronzeCoin, 8:silverCoin
 INIT_WAGON = []
 
 WAGON_INDEX_GENERAL = 0
